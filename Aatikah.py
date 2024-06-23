cfe486d6b49a702cbaebcffd09199cbfeb70e303
Khamis <93088762+Khmys@users.noreply.github.com>
2024-06-23 07:01:05 +0300

    Update Aatikah.py

--- Aatikah.py
+++ Aatikah.py
@@ -1,39 +1,89 @@
 import logging
-from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
-from config import TOKEN  # Ingiza token kutoka config.py
+import asyncio
+import threading
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import swtr
+from config import TOKEN
 
-# Kuweka log level kwenye INFO
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-logging.getLogger("httpx").setLevel(logging.WARNING)
-logger = logging.getLogger(__name__)
 
-# Handler ya amri ya /start
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Tuma ujumbe wakati amri ya /start inapotolewa."""
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Habari #Marafiki tuko hapa kujifunza na kufanya majaribio ya Code au Msimbo")
 
-# Handler ya ujumbe ili kurudia ujumbe
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Rudia ujumbe wa mtumiaji."""
-    await context.bot.copy_message(chat_id=update.effective_chat.id, from_chat_id=update.effective_chat.id, message_id=update.message.id)
+# Command handler kwa amri ya /start
+def start_command(update, context):
+    fname = update.message.chat.first_name
+    atxt = f"Karibu {fname}! Tuma ujumbe nitautafsiri kwa Kiswahili.\n\nTuma picha yenye maelezo (caption) na nitaitafsiri caption yake kuwa Kiswahili.\n\nTuma video yenye maelezo (caption) na nitaitafsiri caption yake kuwa Kiswahili.\n\nKwa sasa naweza kufanya tafsiLugha zote kwenda Kiswahili tu.\n\nKwa msaada zaidi, wasiliana na @Huduma."
+    update.message.reply_text(atxt)
 
-# Handler ya makosa
-async def error_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    logger.warning('Update "%s" ilisababisha kosa "%s"', update, context.error)
 
-# Kazi kuu ya kuanzisha na kuendesha bot
-def main() -> None:
-    application = Application.builder().token(TOKEN).build()
+def tr_command(update, context):
+    message = update.message.reply_to_message
 
-    start_handler = CommandHandler('start', start)
-    echo_handler = MessageHandler(filters.ALL & filters.ChatType.PRIVATE, echo)
+    if message:
+        text = message.text
+        caption = message.caption
 
-    application.add_handler(start_handler)
-    application.add_handler(echo_handler)
-    application.add_error_handler(error_handler)
+        try:
+            swtr.tr_command(update, context)
+        except Exception as e:
+            update.message.reply_text(text="Tafsiri haikufanikiwa. Jaribu tena baadaye.")
 
-    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+
+
+
+# Tafsiri ujumbe wa maandishi
+def translate_to_swahili(update, context):
+        swtr.tr_text(update, context)
+
+
+
+# Tafsiri picha au video na tuma
+def translate_and_send(update, context):
+    if update.message.caption:
+        try:
+            swtr.tr_picha_video(update, context)
+        except Exception as e:
+            update.message.reply_text(text="Tafsiri haikufanikiwa. Tumia Command hii /tr kwa ku, reply message unayotaka kuitranslate.")
+
+
+
+
+def main():
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO
+    )
+    # Unda Updater na Token yako ya boti
+    updater = Updater(TOKEN, use_context=True)
+
+    # Pata kitanzi cha Dispatcher
+    dispatcher = updater.dispatcher
+
+    # Unda handler ya amri ya /start
+    start_handler = CommandHandler('start', start_command)
+    dispatcher.add_handler(start_handler)
+
+    translate_handler = CommandHandler('tr', tr_command)
+
+    # Add command handlers to dispatcher
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(translate_handler)
+
+
+    # Unda handler ya ujumbe wa maandishi
+    message_handler = MessageHandler(Filters.text, translate_to_swahili)
+    dispatcher.add_handler(message_handler)
+
+    # Unda handler ya picha na video
+    media_handler = MessageHandler(Filters.photo | Filters.video, translate_and_send)
+    dispatcher.add_handler(media_handler)
+
+    dispatcher.add_handler(MessageHandler(Filters.animation | Filters.document, translate_and_send))
+
+    # Anza boti
+    updater.start_polling()
+
+    # Endesha boti hadi kusitishwa
+    updater.idle()
 
 if __name__ == '__main__':
     main()
